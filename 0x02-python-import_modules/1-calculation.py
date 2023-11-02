b958556ec1a786660008a89b5303e3d26b0884c1
Epic-Pro <morrismulela@gmail.com>
2023-11-02 09:47:45 -0700

    update

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+def add(a, b):
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return int(a / b)
